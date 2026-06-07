6333dd71f3e0391a9db1cb7b47fdbf60f764081a
jarrios0711 <jarrios0711@gmail.com>
2026-06-07 18:56:17 -0300

    fix: remove solid background from dashboard layout wrapper to expose body background image

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -351,7 +351,7 @@ function GlobalCopilot() {
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex min-h-screen bg-[#050505] text-white">
+        <div className="flex min-h-screen text-white">
             <ParticleBackground />
             <Sidebar />
             <main className="flex-1 relative z-10 pb-20 lg:pb-0">
